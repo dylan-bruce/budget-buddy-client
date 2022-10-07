b049fc669e41a4180ce456c7eb58fbbf5baaa37b
Dylan Bruce <dyanb1127@gmail.com>
2022-10-07 11:43:35 -0400

    Cleaning commented code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
-/*import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import DatePicker from "react-datepicker";*/
 
 
 import './App.css';
